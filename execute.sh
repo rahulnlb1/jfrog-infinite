3a1cec84599a851e63ec014c5a4298febca67b24
Rahul Jain <54938495+rahulnlb1@users.noreply.github.com>
2021-02-24 12:19:13 +0530

    Update execute.sh

--- execute.sh
+++ execute.sh
@@ -1,6 +1,4 @@
-F() { 
-  echo $1
-  F hello
-}
-
-F hello
+for i in $(seq 1 $1) #for as many times, as the first argument ($1) defines...
+do 
+echo "$i $((RANDOM%$2))" #print the current iteration number and a random number in [0, $2)
+done
