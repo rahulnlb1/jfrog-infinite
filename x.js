0f0c9a460c31c67874dd736958cad793f3d28814
Rahul Jain <54938495+rahulnlb1@users.noreply.github.com>
2021-02-25 11:45:27 +0530

    Update x.js

--- x.js
+++ x.js
@@ -1,5 +1,20 @@
-var x = [];
+// var x = [];
 
-while (1) {
-    x.push("hello");
+// while (1) {
+//     x.push("hello");
+// }
+'use strict';
+const list = [];
+setInterval(()=> {
+  const record = new MyRecord();
+  list.push(record);
+},10);
+function MyRecord() {
+  var x='hii';
+  this.name = x.repeat(10000000);
+  this.id = x.repeat(10000000);
+  this.account = x.repeat(10000000);
 }
+setInterval(()=> {
+  console.log(process.memoryUsage())
+},100);
